d9e303acf0de262f5c8c238bf89975b3f8659c12
John Branigan <jb@ulysses.attlocal.net>
2017-04-14 17:08:37 -0500

    Refactor to use knockout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,50 @@
 $(function() {
-    initMap();
+    init();
 });
+
+var Place = function(data) {
+    this.title = ko.observable(data.attributes.RESNAME);
+    this.id = ko.observable(data.attributes.NRIS_Refnum);
+    this.location = {};
+    this.location.lat = data.geometry.y;
+    this.location.lng = data.geometry.x;
+    this.color = function() {
+        if (this.title.includes("House")) {
+            return mapData.colors.house;
+        } else if (name.includes('Church') || name.includes('Temple')) {
+            return mapData.colors.religious;
+        } else if (name.includes('School')) {
+            return mapData.colors.school;
+        } else {
+            return mapData.colors.default;
+        }
+    };
+};
+
+var ListVm = function() {
+    var self = this;
+
+    this.placeList = ko.observableArray([]);
+
+    this.currentPlace = ko.observable();
+
+    this.highlightPlace = function(place) {
+        this.currentPlace(place);
+    };
+};
+
+var InfoVm = function() {
+    this.div = document.getElementById('lightbox');
+};
+
+var viewModel = {};
+
+var init = function() {
+    viewModel.map = new MapVm();
+    viewModel.info = new InfoVm();
+    viewModel.list = new ListVm();
+
+    ko.applyBindings(viewModel);
+    
+    viewModel.map.initMap();
+};
---
+++ js/data.js
@@ -0,0 +1,195 @@
+var mapData = {
+    colors: { 
+        default: '#d18779',
+        house: '#527379',
+        church: '#4c6b54',
+        school: '#d3aa50'
+    },
+    mapStyles: [
+        {
+            "featureType": "all",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "weight": "2.00"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#9c9c9c"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#fcf8f1"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#fcf8f1"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape.man_made",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#fcf8f1"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "saturation": -100
+                },
+                {
+                    "lightness": 45
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#b8b19f"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#575041"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#fcf8f1"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#46bcec"
+                },
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#c8d7d4"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#070707"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        }
+    ]
+};
+
+function loadData(map) {
+    var baseUrl = 'https://mapservices.nps.gov/arcgis/rest/services/cultural_resources/nrhp_locations/MapServer/0/';
+    var queryStr = 'query?outFields=RESNAME%2C+NRIS_Refnum&returnGeometry=true&f=json&geometry=';
+    var bounds = map.getBounds();
+    var extent = bounds.getSouthWest().lng() + ',' + bounds.getSouthWest().lat() + ',' +
+        bounds.getNorthEast().lng() + ',' + bounds.getNorthEast().lat();
+    var request = baseUrl + queryStr + extent;
+
+    $.getJSON(request, function(data) {
+        // TODO: handle errors
+        mapData.data = data.features;
+        mapData.data.forEach(function(place) {
+            viewModel.list.placeList.push( new Place(place) );
+        });
+    });
+}
